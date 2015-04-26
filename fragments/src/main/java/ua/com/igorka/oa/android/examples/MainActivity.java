53ce1c139637a7e16d9fb95285cd2892d3f7e6bc
igor9052 <igor905.2@gmail.com>
2015-04-26 18:27:36 +0300

    Log messages were added to MainActivity to see activity lifecycle.

--- fragments/src/main/java/ua/com/igorka/oa/android/examples/MainActivity.java
+++ fragments/src/main/java/ua/com/igorka/oa/android/examples/MainActivity.java
@@ -1,8 +1,10 @@
 package ua.com.igorka.oa.android.examples;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,10 +12,13 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements CalcKeyboardFragment1.OnCalcButtonPressedListener {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     TextView calcScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, AppCycle.ON_CREATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calcScreen = (TextView) findViewById(R.id.calc_screen);
@@ -23,6 +28,53 @@ public class MainActivity extends ActionBarActivity implements CalcKeyboardFragm
                 .commit();
     }
 
+    @Override
+    protected void onStart() {
+        Log.d(TAG, AppCycle.ON_START);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, AppCycle.ON_RESUME);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        Log.d(TAG, AppCycle.ON_POST_RESUME);
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, AppCycle.ON_PAUSE);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, AppCycle.ON_STOP);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, AppCycle.ON_DESTROY);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, AppCycle.ON_RESTORE_INSTANCE_STATE);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, AppCycle.ON_SAVE_INSTANCE_STATE);
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,4 +126,19 @@ public class MainActivity extends ActionBarActivity implements CalcKeyboardFragm
     public void onCalcButtonPressed(String keyValue) {
         calcScreen.setText(calcScreen.getText().toString() + keyValue);
     }
+
+    private static final class AppCycle {
+        public static final String ON_CREATE = "ON_CREATE";
+        public static final String ON_START = "ON_START";
+        public static final String ON_RESUME = "ON_RESUME";
+        public static final String ON_POST_RESUME = "ON_POST_RESUME";
+        public static final String ON_PAUSE = "ON_PAUSE";
+        public static final String ON_STOP = "ON_STOP";
+        public static final String ON_DESTROY = "ON_DESTROY";
+        public static final String ON_SAVE_INSTANCE_STATE = "ON_SAVE_INSTANCE_STATE";
+        public static final String ON_RESTORE_INSTANCE_STATE = "ON_RESTORE_INSTANCE_STATE";
+
+    }
 }
+
+
